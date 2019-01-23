175d528044ddeb8c8780fa3abd4dc350dc695a4c
unknown <beniciva000@warren.k12.in.us>
2019-01-23 13:42:57 -0500

    time

--- Variables/lesson2.js
+++ Variables/lesson2.js
@@ -2,41 +2,69 @@
 
 $(document).ready(function(){
 
-    $("#mathWrong").click(function(){
- 
-       wrong();
- 
-    })
-    $("#mathRight").click(function(){
- 
-      right();
+   $("#add").click(function(){
+      add();
+   })
 
+   $("#divide").click(function(){
+      divide();
+   })
+   $("#modulus").click(function(){
+      modulus();
    })
     
  });
 
- var wrong = function(){
+//  var wrong = function(){
 
-    var x = $("#number1").val();
-    var y = $("#number2").val();
+//     var x = $("#number1").val();
+//     var y = $("#number2").val();
     
-    var sum = x + y;
-    var message = x + " + " + y + " = " + sum;
+//     var sum = x + y;
+//     var message = x + " + " + y + " = " + sum;
 
-    $("#output").text(message + " ALL information from the user is considered a STRING datatype. The '+' signs means to put the two strings togther." ).show()
+//     $("#output").text(message + " ALL information from the user is considered a STRING datatype. The '+' signs means to put the two strings togther." ).show()
 
- }
+//  }
 
-var right = function(){
+var add = function(){
    var x = $("#number1").val();
    var y = $("#number2").val();
    
    x = parseInt(x);
    y = parseInt(y);
+
    var sum = x + y;
    var message = x + " + " + y + " = " + sum;
 
    $("#output").text(message + " ALL information from the user is considered a STRING datatype. The '+' signs means to put the two strings togther." ).show()
+}
 
+var divide = function(){
 
+   var x = $("#number1").val();
+   var y = $("#number2").val();
+   
+   // x = parseInt(x);
+   // y = parseInt(y);
+
+   var quotient = x / y;
+   $("#output").text(quotient);
+
+}
+
+var modulus = function(){
+
+   var x = $("#number1").val();
+   var y = $("#number2").val();
+
+   var modulus = x % y;
+   $("#output").text(modulus);
+}
+
+var time = function (){
+
+   //x will be converting from days to hours/minutes/seconds
+   //y will be converting from seconds to days
+   
 }
